c7e0f096a3806f3632b725cb25629fe0c7d727c4
yangguanghaitan <2898666969@qq.com>
2022-07-12 10:58:01 +0800

    swagger集成

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,18 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2022-07-12
  */
-@RestController
-@RequestMapping("/eduservice/edu-teacher")
+@Api(description = "讲师模块")
+@Controller
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
 
+    @Autowired
+    EduTeacherService eduTeacherService;
+
+    @ApiOperation(value = "查询所有讲师")
+    @RequestMapping(value = "/listAll",method = RequestMethod.GET)
+    @ResponseBody
+    public List<EduTeacher> listAll(){
+        List<EduTeacher> listTeachers = eduTeacherService.list(null);
+        return listTeachers;
+    }
+
+    @ApiOperation(value = "逻辑删除讲师")
+    @RequestMapping(value = "/deleteById/{ida}",method = RequestMethod.GET)
+    @ResponseBody
+    public Boolean deleteById(@ApiParam(name = "ida",value = "讲师id1",required = true) @PathVariable("ida")String id){
+        System.out.println(id);
+        boolean flag = eduTeacherService.removeById(id);
+        return flag;
+    }
 }
 
