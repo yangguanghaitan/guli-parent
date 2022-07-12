3abd27469e5d1f8aa5e36345ce34e6fe33e110c0
yangguanghaitan <2898666969@qq.com>
2022-07-12 09:26:58 +0800

    生成器生成的代码，没有动

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-07-12
+ */
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+}
+
